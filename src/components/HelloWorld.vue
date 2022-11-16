7d605822bb2e7f7ce2f710fa6a15da0105d112f8
Diogo Eleutero <desdiogo10@yahoo.com>
2022-11-16 11:09:09 -0300

    remove text pwa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,6 @@ defineProps<{
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-      <span>PWa</span>
     </h3>
   </div>
 </template>
