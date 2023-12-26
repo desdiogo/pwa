bae0fbd753be2cded706aeeee026519a5567a4ed
Diogo Eleutero <71910114+desdiogo@users.noreply.github.com>
2023-12-26 15:26:51 -0300

    Update TheWelcome.vue

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,7 +12,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentations</template>
+    <template #heading>Documentation</template>
 
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
