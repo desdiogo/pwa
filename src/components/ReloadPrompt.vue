1af15ace0f04db17b3ad4beb152e6e3ca42ae59c
Diogo Eleutero <desdiogo10@yahoo.com>
2025-10-07 14:38:29 -0300

    Refactor ReloadPrompt.vue to improve service worker update logic and update button styles

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -1,34 +1,60 @@
 <script setup lang="ts">
 import { useRegisterSW } from 'virtual:pwa-register/vue';
-import { ref, onBeforeUnmount, watch } from 'vue';
-import { RssIcon } from 'lucide-vue-next'
+import { ref, watch, onBeforeUnmount } from 'vue';
 import UpdateDialog from '@/components/UpdateDialog.vue';
+import { Rss } from 'lucide-vue-next';
 
-const intervalMs = 1000 * 5
+
+const intervalMs = 5 * 1000
 const updateInterval = ref<ReturnType<typeof setInterval>>();
 
 const { updateServiceWorker, needRefresh, offlineReady } = useRegisterSW({
   onRegisteredSW(swUrl, r) {
-    console.log(`Service Worker at: ${swUrl}`)
-      }
+    if (r) {
+      updateInterval.value = setInterval(async () => {
+        if (r.installing || !navigator) return;
+        if (('connection' in navigator) && !navigator.onLine) return;
+
+        const resp = await fetch(swUrl, {
+          cache: 'no-store',
+          headers: {
+            'cache': 'no-store',
+            'cache-control': 'no-cache',
+          },
+        });
+
+        if (resp?.status === 200) {
+          await r.update();
+        }
+      }, intervalMs);
+    }
+  },
+
+  onNeedRefresh() {
+    console.log('Nova versão detectada');
+  },
+
+  onOfflineReady() {
+    console.log('App pronto offline');
+  }
 });
 
 const open = ref(false);
 
-async function handleClose() {
+// Quando needRefresh muda para true, abre o dialog
+watch(needRefresh, (refresh) => {
+  if (refresh) {
+    open.value = true;
+  }
+});
+
+function handleClose() {
   open.value = false;
 }
 
 function handleUpdate() {
-  updateServiceWorker();
-  open.value = false;
+  updateServiceWorker(true);
 }
-
-watch(needRefresh, (refresh) => {
-  if (refresh) {
-    open.value = refresh;
-  }
-});
 
 onBeforeUnmount(() => {
   if (updateInterval.value) {
@@ -38,10 +64,8 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <span>offlineReady: {{ offlineReady }}</span>
-  <span>needRefresh: {{ needRefresh }}</span>
   <button class="upload-button" :disabled="!needRefresh" @click="open = true">
-    <RssIcon />
+   <Rss />
   </button>
   <UpdateDialog
     v-model:open="open"
@@ -58,6 +82,8 @@ onBeforeUnmount(() => {
   justify-content: center;
   width: 2rem;
   height: 2rem;
+  background: var(--button-primary-bg);
+  color: var(--button-primary-text);
   border-radius: 8px;
   border: none;
   transition: 0.2s;
@@ -69,7 +95,7 @@ onBeforeUnmount(() => {
 }
 
 .upload-button:hover:not(:disabled) {
-  background-color: gray;
+  background-color: var(--button-primary-bg-hover);
 }
 
 .upload-button:disabled {
